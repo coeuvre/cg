2eb17cf63708347cd52d89efdb3a84ffcd965a6c
Coeuvre Wong <coeuvre@gmail.com>
2017-02-22 23:51:30 +0800

    Updated

--- src/core/platform/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef CG_CORE_PLATFORM_LOG_H
-#define CG_CORE_PLATFORM_LOG_H
-
-#include <cg/core/Prim.h>
-
-void cgPlatformLog(char *message);
-
-#endif // CG_CORE_PLATFORM_LOG_H
--- src/core/platform/macos/log.c
+++
@@ -1,8 +0,0 @@
-#include "core/platform/Log.h"
-
-#include <stdio.h>
-
-void cgPlatformLog(char *message)
-{
-    printf("%s\n", message);
-}
